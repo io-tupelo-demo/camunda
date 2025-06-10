6ecc0df17d98a6ff806409f47b014143dd616bbd
Alan Thompson <cloojure@gmail.com>
2025-06-09 22:38:56 -0700

    misc

--- test/tst/demo/core.clj
+++ test/tst/demo/core.clj
@@ -1,15 +1,54 @@
 (ns tst.demo.core
-  (:use demo.core tupelo.core tupelo.test)
+  (:use demo.core
+        tupelo.core
+        tupelo.test)
   (:require
     [tupelo.string :as str]
-    ))
+    )
+  (:import
+    [org.camunda.bpm.client ExternalTaskClient])
+  )
+
+; Maybe needed in BPMN file
+; <camunda:executionListener class="org.camunda.qa.MyExecutionListener" event="start" />
+
+(defn handler
+  [externalTask externalTaskService]
+  (spy :handler--enter)
+  ; Put your business logic here
+
+  ; Get a process variable
+  (let [item   (.getVariable externalTask "item") ; String
+        amount (.getVariable externalTask "amount") ; Integer
+        ]
+
+    (println (format "Charging credit card with an amount of '%s' for the item '%s'..."
+                     amount item))
+
+    ;try {
+    ;     Desktop.getDesktop () .browse (new URI ("https://docs.camunda.org/get-started/quick-start/complete")) ;
+    ;     } catch (Exception e) {
+    ;                            e.printStackTrace () ;
+    ;                            }
+
+    ; Complete the task
+    (spyx (.complete externalTaskService externalTask)))
+  (spy :handler--enter))
 
 (verify
-    (is= 5 (add2 2 3)) ; call a function from demo.core
-    (isnt= 99 (+ 2 3)) ; do a local calculation
-    (is (string? "hello")) ; call a predicate function & verify
+  (let [client (it-> (ExternalTaskClient/create)
+                 (.baseUrl it "http://localhost:8080/engine-rest")
+                 (.asyncResponseTimeout it 10000) ; long polling timeout
+                 (.build it))
+        ]
+    (spyxx client)
+    ; subscribe to an external task topic as specified in the process
+    (it-> client
+      (.subscribe it "charge-card")
+      (.lockDuration it 1000) ; the default lock duration is 20 seconds, but you can override this
+      (.handler it handler)
+      (.open it)
+      )
 
-    (throws? (/ 1 0))
-    (throws-not? (+ 2 3))
-    )
+    ))
 
