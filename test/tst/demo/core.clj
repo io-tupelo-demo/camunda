7bbc19d07adc2490c4d6a6df3a9f4576d23a8d17
Alan Thompson <cloojure@gmail.com>
2025-06-09 20:10:49 -0700

    misc

---
+++ test/tst/demo/core.clj
@@ -0,0 +1,15 @@
+(ns tst.demo.core
+  (:use demo.core tupelo.core tupelo.test)
+  (:require
+    [tupelo.string :as str]
+    ))
+
+(verify
+    (is= 5 (add2 2 3)) ; call a function from demo.core
+    (isnt= 99 (+ 2 3)) ; do a local calculation
+    (is (string? "hello")) ; call a predicate function & verify
+
+    (throws? (/ 1 0))
+    (throws-not? (+ 2 3))
+    )
+
