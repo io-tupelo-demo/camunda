cf2fe7a4fd08246627f08d767fd63858b5770877
Alan Thompson <athom555@mcna.net>
2025-06-12 21:31:05 -0700

    misc

---
+++ go.bash
@@ -0,0 +1,7 @@
+#!/bin/bash 
+
+
+curl -H "Content-Type: application/json" -X POST  \
+  -d '{"variables": {"amount": {"value":345,"type":"integer"}, "item": {"value":"item-xyz"} } }'  \
+  http://localhost:8080/engine-rest/process-definition/key/payment-process/start
+
