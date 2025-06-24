fe7876d42733a2fff8d8cefab0a5028ad242e39f
Alan Thompson <athom555@mcna.net>
2025-06-23 22:04:16 -0700

    misc

--- go2.bash
+++ go2.bash
@@ -2,6 +2,7 @@
 
 set -v
 
+# doesn't have {"businessKey":"alanKey"} value but still works (displays as "null")
 curl -H "Content-Type: application/json" -X POST  \
   -d '{"variables": {"bucket": {"value":"buck"}, "key": {"value":"sample.txt"} } }'  \
   http://localhost:8080/engine-rest/process-definition/key/aaa-newfile-process/start
