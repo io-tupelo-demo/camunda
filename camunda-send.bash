bd710580935185350626ccf847b0ced1e5b2923e
Alan Thompson <athom555@mcna.net>
2025-08-13 12:10:31 -0700

    misc

--- camunda-send.bash
+++ camunda-send.bash
@@ -3,3 +3,4 @@ set -v
 
 curl  -X POST  -H "Content-Type: application/json"  -d @data.json  \
   http://${IP_ADDR_CAMUNDA:-localhost}:8080/engine-rest/process-definition/key/load-834-to-marklogic/start
+
