4e84f96aaaa1dd16ed78172a45189717ada56837
Alan Thompson <athom555@mcna.net>
2025-08-14 12:01:41 -0700

    misc

--- src/finch/camunda_task_02.clj
+++ src/finch/camunda_task_02.clj
@@ -12,33 +12,30 @@
   "Modify 834 filename for XML conversion output"
   [fname-834 :- s/Str]
   (it-> fname-834
-              (str/replace it "." "-")
-              (str it ".xml")))
+    (str/replace it "." "-")
+    (str it ".xml")))
 
 (defn handler-02
   [externalTask externalTaskService]
   (prn :finch.camunda-task-02/handler-02--enter)
   ; Get a process variable (vars defined in BPMN file)
-  (let [fname     (.getVariable externalTask "filename")
-        fname-xml (it-> fname
-                    (str/replace it "." "-")
-                    (str it ".xml"))
-        ]
-    (when true     ; debug
+  (let [fname-834 (.getVariable externalTask "filename")
+        fname-xml (filename-834->xml fname-834)]
+    (when true ; debug
       (nl)
       (spyx-pretty :finch.camunda-task-02/handler-02--vars
-        fname)
+        (vals->map fname-834 fname-xml))
       (nl))
 
-    (let [cmd-str (str "ls -ldF " fname)
-          result  (misc/shell-cmd cmd-str)]
+    (let [r1     (misc/shell-cmd (str "emerald  x12n-to-xml " fname-834 fname-xml))
+          result (misc/shell-cmd (str "ls -ldF " fname-834))]
       (spyx-pretty result))
 
     ; "global" output variable defined on the process, not in BPMN file
     (let [vars {"result" "complete"}]
       (.complete externalTaskService externalTask vars))
     (Thread/sleep 222)
-  (prn :finch.camunda-task-02/handler-02--leave)))
+    (prn :finch.camunda-task-02/handler-02--leave)))
 
 (defn create
   "Create and activate the task on the Camunda server."
