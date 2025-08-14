581cc5ce212b654a34533898f76521cf747a2530
Alan Thompson <athom555@mcna.net>
2025-08-14 11:43:35 -0700

    misc

--- src/finch/camunda_task_02.clj
+++ src/finch/camunda_task_02.clj
@@ -6,16 +6,28 @@
     [finch.config :as config]
     [schema.core :as s]
     [tupelo.misc :as misc]
-    ))
+    [tupelo.string :as str]))
+
+(s/defn filename-834->xml :- s/Str
+  "Modify 834 filename for XML conversion output"
+  [fname-834 :- s/Str]
+  (it-> fname-834
+              (str/replace it "." "-")
+              (str it ".xml")))
 
 (defn handler-02
   [externalTask externalTaskService]
   (prn :finch.camunda-task-02/handler-02--enter)
   ; Get a process variable (vars defined in BPMN file)
-  (let [fname     (.getVariable externalTask "filename")]
+  (let [fname     (.getVariable externalTask "filename")
+        fname-xml (it-> fname
+                    (str/replace it "." "-")
+                    (str it ".xml"))
+        ]
     (when true     ; debug
       (nl)
-      (spyx-pretty :finch.camunda-task-02/handler-02--fname fname)
+      (spyx-pretty :finch.camunda-task-02/handler-02--vars
+        fname)
       (nl))
 
     (let [cmd-str (str "ls -ldF " fname)
---
+++ test/tst/finch/camunda_task_02.clj
@@ -0,0 +1,12 @@
+(ns tst.finch.camunda-task-02
+  (:use finch.camunda-task-02 tupelo.core tupelo.test)
+  (:require
+    [tupelo.misc :as misc]
+    [tupelo.string :as str]
+    ))
+
+(verify
+  (is= "sample-file-834.xml"
+    (filename-834->xml "sample-file.834"))
+  )
+
